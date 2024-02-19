6cc898e8854cd81ef7cf80137b31c39df36e6131
joshva2104 <49963261+joshva2104@users.noreply.github.com>
2024-02-18 17:14:03 -0700

    changed celery timezone

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -392,4 +392,5 @@ AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
 ############################ Celery ############################
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = 'America/Denver'
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
